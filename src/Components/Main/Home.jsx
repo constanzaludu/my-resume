a144d82e3d84b40d95f38281650345801e6dff97
Constanza <constanza.ludu@gmail.com>
2021-12-13 12:04:11 -0300

    Eliminacion de hoja de intro y cambio de nombre de css

--- src/Components/Main/Home.jsx
+++ src/Components/Main/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import Intro from "./Intro";
-
+import "./Home.css";
+import { Container } from 'react-bootstrap';
 
 
 const Home = () => {
-  return (
-    <Intro />
-      
-  );
+  return ( 
+    <>
+        <Container className="Container-name">
+            <div className='Intro'>
+                    <h2>I'm Constanza Luduena.</h2>
+                    <p>I'm a Full Stack developer, originally from Argentina 🇦🇷, but currently located in Spain 🇪🇸.</p>
+                    <p>Curiosity has always been my biggest friend.</p>
+            </div>
+        </Container>
+
+    </>
+    );
 };
 
 export default Home;
