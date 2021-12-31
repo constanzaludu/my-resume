5f4b255bb4ca5f58ba5e23b33d98db836e93279f
Constanza <constanza.ludu@gmail.com>
2021-12-31 11:21:36 -0300

    Se realizo la traduccion en los 3 idiomas de las paginas de Inicio y Sobre mi

--- src/Components/Main/About/Card.jsx
+++ src/Components/Main/About/Card.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import "./Card.css";
 import { Card, Button } from 'react-bootstrap';
 import PhotoCV from "../../../images/cv-photo.jpg";
-import Printer from "../../../images/printer.png"
+import Printer from "../../../images/printer.png";
+import { useTranslation } from "react-i18next";
+
 
 const CardAbout = () => {
+
+    const [t, i18n] = useTranslation("global");
+
     return ( 
         <Card className="tarjeta">
             <Card.Img variant="top" src={PhotoCV} />
             <Card.Body className="text-center">
-                <Card.Title>Constanza Luduena</Card.Title>
-                <Card.Text>
-                Full Stack developer.
-                </Card.Text>
-                <Button className="boton" variant="outline-secondary"> 
-                    <img src={Printer} />
-                Print resume</Button>
+                <Card.Title>{t("card.name")}</Card.Title>
+                <Card.Text>{t("card.post")}</Card.Text>
+                <Button className="boton" variant="outline-secondary"><img src={Printer} />{t("card.button")}</Button>
             </Card.Body>
         </Card>
      );
