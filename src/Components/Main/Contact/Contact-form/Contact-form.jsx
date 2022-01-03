aae3fd99f1065f84d0b100cb2c909f049b5e0fe9
Constanza <constanza.ludu@gmail.com>
2022-01-03 06:19:57 -0300

    cambios de traduccion en json de

--- src/Components/Main/Contact/Contact-form/Contact-form.jsx
+++ src/Components/Main/Contact/Contact-form/Contact-form.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
-import './Contact-form.css'
+import './Contact-form.css';
+import { useTranslation } from "react-i18next";
+
 
 const ContactForm = () => {
+
+    const [t, i18n] = useTranslation("global");
+
     return ( 
         <>
         <Form className="contenedor-contact">
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
+                <Form.Label>{t("contact-form.email")}</Form.Label>
+                <Form.Control type="email" placeholder={t("contact-form.example")} />
+                <Form.Text className="text-muted">{t("contact-form.share")}</Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Write your message</Form.Label>
+                <Form.Label>{t("contact-form.write")}</Form.Label>
                 <Form.Control className="my-text-area" as="textarea" rows={3} />
             </Form.Group>
             <Button className='boton-send' variant="outline-secondary" type="submit">
-                Send
+                {t("contact-form.send")}
             </Button>
         </Form>
         </>
--- src/Components/Main/Contact/Contact-info/Contact-info.jsx
+++ src/Components/Main/Contact/Contact-info/Contact-info.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import './Contact-info.css'
+import './Contact-info.css';
+import { useTranslation } from "react-i18next";
+
 
 const ContactInfo = () => {
+    
+    const [t, i18n] = useTranslation("global");
+
     return ( 
         <>
         <div className="container-contact">
-            <h2 className="text-center">Contact information</h2>
-            <p>Email address: constanza.ludu@gmail.com</p>
-            <p>Cellphone number: +34 641033789</p>
-            <p>WhatsApp: +54 0236 154418354</p>
-            <p>Country of origin: Argentina</p>
-            <p>Country of residence: Spain</p>
+            <h2 className="text-center">{t("contact-info.info")}</h2>
+            <ul>
+                <li>{t("contact-info.email")}<span>: constanza.ludu@gmail.com</span></li>
+                <li>{t("contact-info.phone")}<span>: +34 641033789</span></li>
+                <li>{t("contact-info.wpp")}<span>: +54 0236 154418354</span></li>
+                <li>{t("contact-info.origen")}<span>: {t("contact-info.argentina")}</span></li>
+                <li>{t("contact-info.residence")}<span>: {t("contact-info.spain")}</span></li>
+            </ul>
         </div>        
         </>
      );
